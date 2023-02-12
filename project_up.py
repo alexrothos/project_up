f34e3e10d298ffa62dbc94ea87687f904bf95427
Alex Rothos <alexrothos@gmail.com>
2023-02-12 11:26:02 +0200

    DB error handling added

--- project_up.py
+++ project_up.py
@@ -69,40 +69,52 @@ def index():
     data = get_data()
     if data is None:
         return "Unsuccessful request", 401
-
-    company, company_employees, offer = data["company"], data["company_employees"], data["offer"]
-
+    try:
+        company, company_employees, offer = data["company"], data["company_employees"], data["offer"]
+    except Exception as e:
+        return "Invalid data : " + str(e), 400
+    
     # Save company data
-    company_obj = Company.query.filter_by(display_name=company["display_name"]).first()
-    if not company_obj:
-        company_obj = Company(display_name=company["display_name"], address_detail=company["address_detail"], city_name=company["city_name"], region_name=company["region_name"], zip_code=company["zip_code"])
-        db.session.add(company_obj)
-        db.session.commit()
-        print('Company saved successfully')
-    else:
-        print('Company already exists')
+    try:
+        company_obj = Company.query.filter_by(display_name=company["display_name"]).first()
+        if not company_obj:
+            company_obj = Company(display_name=company["display_name"], address_detail=company["address_detail"], city_name=company["city_name"], region_name=company["region_name"], zip_code=company["zip_code"])
+            db.session.add(company_obj)
+            db.session.commit()
+            print('Company saved successfully')
+        else:
+            print('Company already exists')
+    except Exception as e:
+        db.rollback()
+        print('Company data not saved : ' + str(e))
 
     # Save company employee data
-    for employee in company_employees:
-        employee_obj = CompanyEmployee.query.filter_by(email=employee["email"]).first()
-        if not employee_obj:
-            employee_obj = CompanyEmployee(company_id=company_obj.id, employee_id=employee["employee_id"], fullname=employee["fullname"], email=employee["email"], phoneNumber=employee["phoneNumber"])
-            db.session.add(employee_obj)
-            db.session.commit()
-            print('Employee saved successfully')
-        else:
-            print('Employee already exists')
+    try:
+        for employee in company_employees:
+            employee_obj = CompanyEmployee.query.filter_by(email=employee["email"]).first()
+            if not employee_obj:
+                employee_obj = CompanyEmployee(company_id=company_obj.id, employee_id=employee["employee_id"], fullname=employee["fullname"], email=employee["email"], phoneNumber=employee["phoneNumber"])
+                db.session.add(employee_obj)
+                db.session.commit()
+                print('Employee saved successfully')
+            else:
+                print('Employee already exists')
+    except Exception as e:
+        db.rollback()
+        print('Employee data not saved : ' + str(e))
 
     # # Save offer data
-    for key in offer:
-        if offer[key] == 0:
-            offer[key] = None
-    offer_obj = Offer(company_id=company_obj.id, product_1_budget=offer["product_1_budget"], product_1_issuing_value=offer["product_1_issuing_value"], product_1_mailing_value=offer["product_1_mailing_value"], product_1_value=offer["product_1_value"], product_2_budget=offer["product_2_budget"], product_2_issuing_value=offer["product_2_issuing_value"], product_2_mailing_value=offer["product_2_mailing_value"], product_2_value=offer["product_2_value"], product_3_budget=offer["product_3_budget"], product_3_issuing_value=offer["product_3_issuing_value"], product_3_mailing_value=offer["product_3_mailing_value"], product_3_value=offer["product_3_value"])
-    db.session.add(offer_obj)
-    db.session.commit()
-    print('Offer saved successfully')
-
-    #db.session.commit()
+    try:
+        for key in offer:
+            if offer[key] == 0:
+                offer[key] = None
+        offer_obj = Offer(company_id=company_obj.id, product_1_budget=offer["product_1_budget"], product_1_issuing_value=offer["product_1_issuing_value"], product_1_mailing_value=offer["product_1_mailing_value"], product_1_value=offer["product_1_value"], product_2_budget=offer["product_2_budget"], product_2_issuing_value=offer["product_2_issuing_value"], product_2_mailing_value=offer["product_2_mailing_value"], product_2_value=offer["product_2_value"], product_3_budget=offer["product_3_budget"], product_3_issuing_value=offer["product_3_issuing_value"], product_3_mailing_value=offer["product_3_mailing_value"], product_3_value=offer["product_3_value"])
+        db.session.add(offer_obj)
+        db.session.commit()
+        print('Offer saved successfully')
+    except Exception as e:
+        db.rollback()
+        print('Offer data not saved : ' + str(e))
 
     return "Data saved successfully", 200
 
